31e822996f5a3688f1ef4f15be3657531d5ad4af
imovius <ian.movius@gmail.com>
2025-08-27 22:48:53 -0700

    simplified project

--- quiz_only/help_scripts.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd, matplotlib.pyplot as plt
-s = pd.read_csv("outputs/famd_scores_with_clusters.csv")
-plt.figure(figsize=(7,5))
-plt.scatter(s["FAMD_1"], s["FAMD_2"], s=8, alpha=0.6, c=s["cluster"])
-plt.xlabel("FAMD 1"); plt.ylabel("FAMD 2"); plt.title("FAMD (colored by cluster)")
-plt.tight_layout(); plt.show()
